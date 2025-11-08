e10da211f00a36e6d416930e9fdee6717284a9fd
IgnaBascu <igna.bascu@gmail.com>
2025-11-08 17:58:45 -0300

    Creación entidades 'Producto' y 'Activo'

---
+++ catalogo-service/src/main/java/com/utabox/catalogo_service/model/Activo.java
@@ -0,0 +1,52 @@
+package com.utabox.catalogo_service.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "activos")
+public class Activo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_activo")
+    private Integer idActivo;
+
+    @Column(name = "nombre_sala", nullable = false, length = 255)
+    private String nombreSala; // ej. "Sala VIP 101"
+
+    @Column(nullable = false, length = 20)
+    private String estado; // ej. "disponible", "mantenimiento"
+
+    // --- Relación ---
+    // Muchos Activos (salas) pertenecen a un Producto (tipo de sala)    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    // --- Getters y Setters ---       
+
+    public String getNombreSala() {
+        return nombreSala;
+    }
+    public void setNombreSala(String nombreSala) {
+        this.nombreSala = nombreSala;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public Producto getProducto() {
+        return producto;
+    }
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getIdActivo() { return idActivo; }
+
+    public void setIdActivo(Integer idActivo) { this.idActivo = idActivo; }
+    
+}
+    
+}
