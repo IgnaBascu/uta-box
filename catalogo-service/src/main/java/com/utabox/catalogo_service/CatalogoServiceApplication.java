3491e3e4184e91335bf9e4833e1cb2c837056f37
IgnaBascu <igna.bascu@gmail.com>
2025-11-08 18:57:10 -0300

    Creacion controladores y agregar servicio a eureka

--- catalogo-service/src/main/java/com/utabox/catalogo_service/CatalogoServiceApplication.java
+++ catalogo-service/src/main/java/com/utabox/catalogo_service/CatalogoServiceApplication.java
@@ -2,8 +2,10 @@ package com.utabox.catalogo_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogoServiceApplication {
 
 	public static void main(String[] args) {
---
+++ catalogo-service/src/main/java/com/utabox/catalogo_service/controller/CatalogoController.java
@@ -0,0 +1,31 @@
+package com.utabox.catalogo_service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.utabox.catalogo_service.model.Producto;
+import com.utabox.catalogo_service.repository.ProductoRepository;
+
+@RestController
+@RequestMapping("api/productos") // Prefijo para todos los endpoints
+public class CatalogoController {
+    
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping("/salas") // Endpoint publico
+    public ResponseEntity<List<Producto>> getTiposDeSala() {
+        
+        List<Producto> salas = productoRepository.findByTipo("sala");   
+        
+        return ResponseEntity.ok(salas);
+    }
+
+
+
+}
